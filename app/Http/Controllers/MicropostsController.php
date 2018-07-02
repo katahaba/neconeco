4b9036e8d5c6443e4e8e979f5fd90e4ce6e82c0c
necocat <noctiluca1234@gmail.com>
2018-07-02 15:22:32 +0000

    post

---
+++ app/Http/Controllers/MicropostsController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MicropostsController extends Controller
+{
+    public function index()
+    {
+        $data = [];
+        if (\Auth::check()) {
+            $user = \Auth::user();//'\'はIlluminate\Support\Facades\Auth;を意味しておりグローバル名前空間というらしい
+            $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+            
+            $data = ['user' => $user,
+            'microposts' => $microposts,
+            ];
+            $data += $this->counts($user);
+            return view('users.show', $data);
+        }else{
+            return view('welcom');
+        }
+    }
+    
+    public function store()
+    {
+        $this->validate($request, ['content'=> 'required|mac:191',]);
+        
+        $request->user()->microposts()->create(['content' => $request->content,]);
+        
+        return redirect()->back;
+    }
+
+     public function destroy($id)
+     {  
+        $micropost = \App\Micropost::find($id);
+
+        if (\Auth::id() === $micropost->user_id) {
+            $micropost->delete();
+        }
+
+        return redirect()->back();
+     }  
+}
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('users.users', ['users' => $users])
+    @include('users.users', ['users' => $users])//後々のために分離させてあるuser.blade.phpをincludeしている
 @endsection
