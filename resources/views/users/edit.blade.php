b50fe6a23938007ea05c6232b468ce091ad9b785
necocat <noctiluca1234@gmail.com>
2018-08-14 17:29:45 +0000

    edit user

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -2,13 +2,14 @@
 
 @section('content')
 
-    <p>id: {{ $user->id }} の名前編集ページ</p>
+    <p>名前編集</p>
     {!! Form::model($user, ['route' => ['users.update', $user->id], 'method' => 'put']) !!}
         {!! Form::label('name', '名前:') !!}
         {!! Form::text('name') !!}
         {!! Form::submit('更新') !!}
     {!! Form::close() !!}
-    <p>id: {{ $user->id }} の登録削除</p>
+    <br>
+    <p>登録削除</p>
     {!! Form::model($user, ['route' => ['users.destroy', $user->id], 'method' => 'delete']) !!}
         {!! Form::submit('削除') !!}
     {!! Form::close() !!}
