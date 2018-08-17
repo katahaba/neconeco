fbdd93f7e7bbf9219acbebec6405021dfb16fb5f
necocat <noctiluca1234@gmail.com>
2018-08-17 09:36:05 +0000

    user panel6

--- resources/views/microposts/all_maps.blade.php
+++ resources/views/microposts/all_maps.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-<img style="width: 350px; height: 350px;" src="https://map.yahooapis.jp/map/V1/static?appid={{ getenv('YAHOO_API_KEY') }}&{{$datas}},&z=6">
+<img class="allmap" " src="https://map.yahooapis.jp/map/V1/static?appid={{ getenv('YAHOO_API_KEY') }}&{{$datas}},&z=6">
 @endsection
