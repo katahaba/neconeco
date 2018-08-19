a8a256d5f5a6819442b9941b7ac186aee4742120
necocat <noctiluca1234@gmail.com>
2018-08-19 15:50:14 +0000

    usage2

--- resources/views/usage.blade.php
+++ resources/views/usage.blade.php
@@ -17,8 +17,14 @@
 <br>
 <br>
 <br>
+<br>
+<br>
+<br>
 <p>5つのタブが現れるので一番左のNew Postをクリックしてください。</p>
 <img src="https://neconeco.s3.us-east-2.amazonaws.com/usage/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202018-08-19%2023.39.16.png?">
+<br>
+<br>
+<br>
 <br>
 <br>
 <br>
@@ -31,8 +37,24 @@
 <br>
 <br>
 <br>
-<p>All Photos、ユーザープロファイルのPhotos、Favo_Photosなどから個々の写真をクリックすると写真のお気に入り、自分のデータなら修正や削除ができます。</p>
+<br>
+<br>
+<br>
+<p>写真一覧の写真はグリッドで配置を変えることができます。更新ボタンを押してもキャッシュに保存されます。</p>
+<img src="https://neconeco.s3.us-east-2.amazonaws.com/usage/%E3%81%82.png?">
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+All Photos、ユーザープロファイルのPhotos、Favo_Photosなどから個々の写真をクリックすると写真のお気に入り、自分のデータなら修正や削除ができます。</p>
 <img src="https://neconeco.s3.us-east-2.amazonaws.com/usage/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202018-08-19%2023.40.58.png?">
+<br>
+<br>
+<br>
 <br>
 <br>
 <br>
@@ -40,6 +62,9 @@
 <br>
 <p>また、ナビバーのAll Neco Mapsから地図上に投稿写真の位置が表示されます(データが登録されていれば)。</p>
 <img src="https://neconeco.s3.us-east-2.amazonaws.com/usage/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202018-08-20%200.16.42.png">
+<br>
+<br>
+<br>
 <br>
 <br>
 <br>
