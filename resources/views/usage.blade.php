b28befbe0db60ea483d5ccf712c592b052389461
necocat <noctiluca1234@gmail.com>
2018-08-20 09:42:05 +0000

    usage7

--- resources/views/usage.blade.php
+++ resources/views/usage.blade.php
@@ -5,7 +5,11 @@
 <br>
 <br>
 <h3>使い方</h3>
-<p>今のところはトップ画面からユーザー名と適当なメールアドレスとパスワードで登録できます。</p>
+<p>トップ画面の「sign up now!」からユーザー名と適当なメールアドレスとパスワードで登録できます。</p>
+<img class="usage"src="https://neconeco.s3.us-east-2.amazonaws.com/usage/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202018-08-20%2018.32.00.png">
+<br>
+<br>
+<br>
 <br>
 <br>
 <br>
@@ -22,7 +26,7 @@
 <br>
 <br>
 <p>5つのタブが現れるので一番左のNew Postをクリックしてください。</p>
-<img class="usage" src="https://neconeco.s3.us-east-2.amazonaws.com/usage/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202018-08-19%2023.39.16.png?">
+<img class="usage" src="https://neconeco.s3.us-east-2.amazonaws.com/usage/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202018-08-20%2018.25.51.png">
 <br>
 <br>
 <br>
@@ -32,7 +36,7 @@
 <br>
 <br>
 <p>写真(必須)、写真検索用タグ(任意)、撮影場所(中央位置,任意)を決めたらUploadボタンを押して下さい。</p>
-<img class="usage"  src="https://neconeco.s3.us-east-2.amazonaws.com/usage/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202018-08-19%2023.39.49.png?">
+<img class="usage"  src="https://neconeco.s3.us-east-2.amazonaws.com/usage/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202018-08-19%2023.39.49.png">
 <br>
 <br>
 <br>
@@ -42,7 +46,7 @@
 <br>
 <br>
 <p>All Photos、ユーザープロファイルのPhotos、Favo_Photosの写真はグリッドで配置を変えることができます。更新ボタンを押してもキャッシュに配置が保存されています。</p>
-<img class="usage" src="https://neconeco.s3.us-east-2.amazonaws.com/usage/%E3%81%82.png?">
+<img class="usage" src="https://neconeco.s3.us-east-2.amazonaws.com/usage/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202018-08-20%2018.24.15.png">
 <br>
 <br>
 <br>
@@ -52,7 +56,7 @@
 <br>
 <br>
 <p>All Photos、ユーザープロファイルのPhotos、Favo_Photosなどから個々の写真をクリックすると写真詳細からお気に入りができ、自分のデータならば修正や削除もできます。</p>
-<img class="usage"src="https://neconeco.s3.us-east-2.amazonaws.com/usage/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202018-08-19%2023.40.58.png?">
+<img class="usage"src="https://neconeco.s3.us-east-2.amazonaws.com/usage/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202018-08-20%2018.27.01.png">
 <br>
 <br>
 <br>
@@ -72,6 +76,6 @@
 <br>
 <br>
 <p>検索バーで試しに「スイカ,子猫」と入力してみるとスイカ、または、子猫のタグで登録された写真が出てくるはずです。(半角,でつなげるとor検索になる。)</p>
-<img class="usage"src="https://neconeco.s3.us-east-2.amazonaws.com/usage/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202018-08-20%200.17.05.png?">
+<img class="usage"src="https://neconeco.s3.us-east-2.amazonaws.com/usage/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202018-08-20%200.17.05.png">
 </div>
 @endsection
